be4d398883eda2f94ebc0e2328015a713caa8d28
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-06 12:29:39 +0200

    Create Datentypen.cpp

---
+++ Semester2/C_CPP/Uebungen_Woche3/Datentypen.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char buchstabe = 'c';
+	int integer = 8;
+	float gleitkomma = 7.3;
+	double doppel = 124567;
+
+	cout << "Size char: " << sizeof(char) << endl;
+	cout << "Size int: " << sizeof(int) << endl;
+	cout << "Size float: " << sizeof(float) << endl;
+	cout << "Size double: " << sizeof(double) << endl;
+}
