149bca706fcdb14b556a0a58c526f72c402532be
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-06 14:39:22 +0200

    Update constante.c
    enum fehlte

--- Semester2/C_CPP/Uebungen_Woche2/constante.c
+++ Semester2/C_CPP/Uebungen_Woche2/constante.c
@@ -2,9 +2,23 @@
 
 #define def_constante 13
 
+enum zahlen {
+	LOW = 25,
+	MEDIUM = 50,
+	HIGH = 75,
+	PEWPEW = 100
+};
+
 int main()
 {
+	enum zahlen myEnum = PEWPEW;
+	printf("ENUM: %d", myEnum);
+
+int main()
+{
+	enum zahlen myEnum = PEWPEW;
 	int const constante = 37;
 	printf("def_constante: %d\n", def_constante);
 	printf("const-constante: %d\n", constante);
+	printf("ENUM: %d\n", myEnum);
 }
