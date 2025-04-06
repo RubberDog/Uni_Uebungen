66f558623ace80203b1cc26d643c195c34d73ab7
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-06 09:45:22 +0200

    Create printf_uebung.c

---
+++ Semester2/C_CPP/Uebungen_Woche2/printf_uebung.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int number = 17;
+	float zahl_pi = 3.14159;
+	char ausgabestring[] = "Python > dieser Kram";
+
+	printf("Integer: %d\n", number);
+	printf("Hexadezimal: %x\n", number);
+	printf("Octal: %o\n", number);
+	printf("Zeichenkette: %s\n\n", ausgabestring);
+}
