03247ffe7381d048aaa2af2fa7e6e2ca61439521
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-06 09:49:17 +0200

    Create constante.c

---
+++ Semester2/C_CPP/Uebungen_Woche2/constante.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define def_constante 13
+
+int main()
+{
+	int const constante = 37;
+	printf("def_constante: %\n", def_constante);
+	printf("const-constante: %d\n", constante);
+
+}
