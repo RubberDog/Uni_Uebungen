a68ce9d0e183693e275b9a8c7d6d8bc8d36b3cc7
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-06 12:12:48 +0200

    Create Hello_World_cout.cpp

---
+++ Semester2/C_CPP/Uebungen_Woche3/Hello_World_cout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number = 1;
+	float number2 = 3.3;
+	const int number3 = 7;
+	char character = 'y';
+
+	cout << "Integer: " << number << endl;
+	cout << "Float: " << number2 << endl;
+	cout << "Const: " << number3 << endl;
+	cout << "Char: " << character << endl;
+
+	return 0;
+}
