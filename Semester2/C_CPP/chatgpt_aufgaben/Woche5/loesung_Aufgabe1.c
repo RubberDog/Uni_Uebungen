36455b12263c3d01620efe57ff613fc87092ee86
Matthias Kümmel <mkuemmel26@gmail.com>
2025-04-28 11:13:56 +0200

    Create loesung_Aufgabe1.c

---
+++ Semester2/C_CPP/chatgpt_aufgaben/Woche5/loesung_Aufgabe1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char password[] = "geheim";
+    char eingabe[200] = " ";
+    while (strcmp(password, eingabe) != 0) {
+        for (int i = 1; i <= 3; i++) {
+            printf("Nenne mir bitte das Passwort: \n");
+            fgets(eingabe, sizeof(eingabe), stdin);
+            eingabe[strcspn(eingabe, "\n")] = '\0';
+            if (strcmp(password, eingabe) != 0) {
+                printf("Leider falsch. Das war Versuch %d\n\n", i);
+                printf("%s, %s", password, eingabe);
+            } else {
+                printf("Zugang gewährt.");
+                return 0;
+            }
+        }
+        printf("Zugang verweigert.");
+    }
+}
