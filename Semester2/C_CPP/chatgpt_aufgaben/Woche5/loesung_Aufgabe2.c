6560cae23067229ec1b4401badd51871a6f48ead
Matthias Kümmel <mkuemmel26@gmail.com>
2025-05-04 14:54:07 +0200

    Update loesung_Aufgabe2.c

--- Semester2/C_CPP/chatgpt_aufgaben/Woche5/loesung_Aufgabe2.c
+++ Semester2/C_CPP/chatgpt_aufgaben/Woche5/loesung_Aufgabe2.c
@@ -2,9 +2,10 @@
 
 
 int main() {
-    int eingabe, summe;
+    int eingabe;
+    int summe = 0;
     printf("Bis zu welcher Zahl moechtest du die geraden Zahlen addieren? \n");
-    scanf("%d", &eingabe);
+    scanf_s("%d", &eingabe);
     for (int i = 1; i <= eingabe; i++) {
         if (i % 2 == 0) {
             summe += i;
