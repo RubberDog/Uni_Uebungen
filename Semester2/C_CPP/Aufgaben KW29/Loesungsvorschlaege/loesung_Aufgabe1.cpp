d110827da96222b8a190a4c735c850b6078a2e99
Matthias Kümmel <mkuemmel26@gmail.com>
2025-07-14 17:39:04 +0200

    Update - getter-Methoden

--- Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe1.cpp
+++ Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 class Punkt {
-public:
+private:
     double x, y;
+public:
     Punkt(){
         x = 0.0;
         y = 0.0;
@@ -17,23 +18,26 @@ public:
     Punkt add(const Punkt&);
     Punkt operator+(const Punkt&);
 
+    double getX() const { return x; }
+    double getY() const { return y; }
+
 };
 
 Punkt Punkt::add(const Punkt& p2) {
-    return Punkt(x + p2.x, y + p2.y);
+    return Punkt(x + p2.getX(), y + p2.getY());
 }
 
 Punkt Punkt::operator+(const Punkt& p2) {
-    return Punkt(x + p2.x, y + p2.y);
+    return Punkt(x + p2.getX(), y + p2.getY());
 }
 
 int main() {
     Punkt p1;
-    Punkt p2(4.5, 4.5);
+    Punkt p2(7.5, 4.5);
     Punkt p3 = p1.add(p2);
     std::cout << "Mit add: \n";
-    std::cout << "p3.x = " << p3.x << ", p3.y = " << p3.y << std::endl;
+    std::cout << "p3.x = " << p3.getX() << ", p3.y = " << p3.getY() << std::endl;
     Punkt p4 = p2+p1;
     std::cout << "Mit operator+: \n";
-    std::cout << "p4.x = " << p4.x << ", p4.y = " << p4.y << std::endl;
+    std::cout << "p4.x = " << p4.getX() << ", p4.y = " << p4.getY() << std::endl;
 }
