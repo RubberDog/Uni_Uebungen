ef6f5897ffcc57f71d21636b4daab5c437faf62b
Matthias Kümmel <mkuemmel26@gmail.com>
2025-07-15 06:47:17 +0200

    Create loesung_Aufgabe2.cpp

---
+++ Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Punkt {
+private:
+    double x,y,z;
+public:
+    Punkt() {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+    }
+    Punkt(double xVal, double yVal, double zVal) {
+        x = xVal;
+        y = yVal;
+        z = zVal;
+    }
+    ~Punkt() {
+    }
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+    double getZ() const { return z; }
+
+    Punkt add(Punkt& p2) {
+        return Punkt(x+p2.getX(), y + p2.getY(), z + p2.getZ());
+    }
+    Punkt operator+(Punkt& p2) {
+        return Punkt(x+p2.getX(), y + p2.getY(), z + p2.getZ());
+    }
+};
+
+int main() {
+    Punkt p9;
+    Punkt p8(6.0,7.0,9.0);
+    Punkt p7 = p8.add(p9);
+    Punkt p6 = p8.operator+(p9);
+    std::cout << "p7.x = " << p7.getX() << ", p7.y = " << p7.getY() << ", p7.z = " << p7.getZ() << std::endl;
+    std::cout << "p6.x = " << p6.getX() << ", p6.y = " << p6.getY() << ", p6.z = " << p6.getZ() << std::endl;
+}
