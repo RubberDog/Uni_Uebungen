944f03ba2f5375af48f0d696a129a26fe0603f1e
Matthias Kümmel <mkuemmel26@gmail.com>
2025-07-18 08:52:30 +0200

    Update loesung_Aufgabe2.cpp

--- Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe2.cpp
+++ Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe2.cpp
@@ -14,17 +14,16 @@ public:
         y = yVal;
         z = zVal;
     }
-    ~Punkt() {
-    }
+    ~Punkt() = default;
 
     double getX() const { return x; }
     double getY() const { return y; }
     double getZ() const { return z; }
 
-    Punkt add(Punkt& p2) {
+    Punkt add(const Punkt& p2) const {
         return Punkt(x+p2.getX(), y + p2.getY(), z + p2.getZ());
     }
-    Punkt operator+(Punkt& p2) {
+    Punkt operator+(const Punkt& p2) const {
         return Punkt(x+p2.getX(), y + p2.getY(), z + p2.getZ());
     }
 };
@@ -33,7 +32,7 @@ int main() {
     Punkt p9;
     Punkt p8(6.0,7.0,9.0);
     Punkt p7 = p8.add(p9);
-    Punkt p6 = p8.operator+(p9);
+    Punkt p6 = p8 + p9;
     std::cout << "p7.x = " << p7.getX() << ", p7.y = " << p7.getY() << ", p7.z = " << p7.getZ() << std::endl;
     std::cout << "p6.x = " << p6.getX() << ", p6.y = " << p6.getY() << ", p6.z = " << p6.getZ() << std::endl;
 }
