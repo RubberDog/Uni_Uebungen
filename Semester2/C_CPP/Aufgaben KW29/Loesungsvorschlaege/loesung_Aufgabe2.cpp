dd80983e7bd8c943bb82e0e3e94730084682ecbc
Matthias Kümmel <mkuemmel26@gmail.com>
2025-07-20 18:51:34 +0200

    Update loesung_Aufgabe2.cpp

--- Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe2.cpp
+++ Semester2/C_CPP/Aufgaben KW29/Loesungsvorschlaege/loesung_Aufgabe2.cpp
@@ -32,6 +32,7 @@ int main() {
     Punkt p9;
     Punkt p8(6.0,7.0,9.0);
     Punkt p7 = p8.add(p9);
+    Punkt p345 = p8.operator+(p9);
     Punkt p6 = p8 + p9;
     std::cout << "p7.x = " << p7.getX() << ", p7.y = " << p7.getY() << ", p7.z = " << p7.getZ() << std::endl;
     std::cout << "p6.x = " << p6.getX() << ", p6.y = " << p6.getY() << ", p6.z = " << p6.getZ() << std::endl;
