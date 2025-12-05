9fd31c2fbfbffffad93289497a0675210c3fc092
Matthias Kümmel <mkuemmel26@gmail.com>
2025-12-05 10:15:42 +0100

    rework mystyle

--- Semester3/Linux/Shell/myquestion.sh
+++ Semester3/Linux/Shell/myquestion.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# Prüfen, ob Frage übergeben wurde
 if [ $# -eq 0 ]; then
-	echo "Fehler: Bitte eine Frage als Parameter übergeben."
-	echo "Beispiel: ./myquestion \"Fortfahren?\""
-	exit 2
+    echo "Keine Frage? Keine Antwort. Geh weg."
+    exit 2
 fi
 
-# Die Frage aus den Parametern zusammensetzen
 frage="$*"
 
 while true; do
-	read -p "$frage (Ja/Nein): " antwort
+    read -p "$frage (Ja/Nein): " antwort
+    antwort=$(echo "$antwort" | tr '[:upper:]' '[:lower:]')
 
-	# Eingabe in lowercaser umwandeln
-	antwort=$(echo "$antwort" | tr '[:upper:]' '[:lower:]')
-
-	# Antwort auswerten
-	
-	case "$antwort" in
-		ja|j)
-			exit 0
-			;;
-		nein|n)
-			exit 1
-			;;
-		*)
-			echo "Bitte mit 'Ja' oder 'Nein' antworten."
-			;;
-	esac
+    case "$antwort" in
+        ja|j)
+            exit 0
+            ;;
+        nein|n)
+            exit 1
+            ;;
+        *)
+            echo "So nich, Brudi"
+            ;;
+    esac
 done
