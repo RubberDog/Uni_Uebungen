c896cf71f5c49dde19a8190365a73bf4517b56a4
Matthias Kümmel <mkuemmel26@gmail.com>
2026-01-18 11:28:39 +0100

    Create klausur_a4.sh

---
+++ Semester3/Linux/Shell/klausur_a4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Keine Dateien zum löschen angegeben. Nix zu tun, tschö mit ö.\n"
+    exit 2
+fi  
+
+for filname in "$@"; do
+    if ./klausur_a3.sh "Datei $filename löschen?\n"; then
+        rm -- "$filename"
+    fi  
+done
